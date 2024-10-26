bbeaf6eef2cfe44cfaf2d1c452a3915e7ef4d66b
Grace <gracelia@andrew.cmu.edu>
2024-10-26 15:10:59 -0400

    Add Swift project to repository

---
+++ rently/rentlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  rentlyApp.swift
+//  rently
+//
+//  Created by Grace Liao on 10/26/24.
+//
+
+import SwiftUI
+
+@main
+struct rentlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

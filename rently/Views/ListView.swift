4e574af731558b64db8573b093d4f50bf4a7608a
Grace <gracelia@andrew.cmu.edu>
2024-10-27 16:51:51 -0400

    Set up tab bar navigation

---
+++ rently/Views/ListView.swift
@@ -0,0 +1,21 @@
+//
+//  ListView.swift
+//  rently
+//
+//  Created by Grace Liao on 10/27/24.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        VStack {
+            Text("This is the list page.")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ListView()
+}
---
+++ rently/Views/SearchView.swift
@@ -0,0 +1,21 @@
+//
+//  SearchView.swift
+//  rently
+//
+//  Created by Grace Liao on 10/27/24.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        VStack {
+            Text("This is the search page.")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    SearchView()
+}

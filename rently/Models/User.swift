325fcd377d284d0963dcbf978460ba7a29275be1
Grace <gracelia@andrew.cmu.edu>
2024-11-01 15:18:13 -0400

    Making User model codable to Firebase

--- rently/Models/User.swift
+++ rently/Models/User.swift
@@ -6,9 +6,10 @@
 //
 
 import Foundation
+import FirebaseFirestore
 
 struct User: Codable, Identifiable {
-    let id: String
+    @DocumentID var id: String? = UUID().uuidString
     var firstName: String
     var lastName: String
     var username: String
@@ -21,6 +22,23 @@ struct User: Codable, Identifiable {
     var likedItems: [String] // Item IDs
     var styleChoices: [String]
     var events: [String]
-    var notifications: [Notification]
+    //var notifications: [Notification]
     //var busySlots: [BusySlot]
+    
+    // MARK: Codable
+    enum CodingKeys: String, CodingKey {
+        case id
+        case firstName = "FirstName"
+        case lastName = "LastName"
+        case username
+        case pronouns = "Pronouns"
+        case email = "Email"
+        case password = "Password"
+        case university = "University"
+        case rating = "Rating"
+        case listings = "ItemListings"
+        case likedItems = "LikedItems"
+        case styleChoices = "StyleChoices"
+        case events = "Events"
+    }
 }

860f3a77be0454301dd945b85d3c0271fcad680b
Abby Chen <archen@andrew.cmu.edu>
2024-11-03 16:52:13 -0500

    Updated UI of make listing first page, began skeleton of second page

---
+++ rently/Views/MakeListingTwoView.swift
@@ -0,0 +1,24 @@
+//
+//  MakeListingTwoView.swift
+//  rently
+//
+//  Created by Abby Chen on 11/3/24.
+//
+
+import SwiftUI
+
+// second page of making a listing
+struct MakeListingTwoView: View {
+  var body: some View {
+    VStack(alignment: .leading, spacing: 16) {
+      // page title
+      Text("make a listing")
+        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+        .bold()
+    }
+  }
+}
+
+#Preview {
+    MakeListingTwoView()
+}

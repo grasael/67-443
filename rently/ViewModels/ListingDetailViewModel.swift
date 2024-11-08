d6ab215b88baed6f8bcc4bfb80623f3da707256a
Tishyaa Chaudhry <tishyaa.chaudhry@gmail.com>
2024-11-08 09:45:54 -0500

    added Sara's files

---
+++ rently/ViewModels/ListingDetailViewModel.swift
@@ -0,0 +1,51 @@
+//
+//  ListingDetailViewModel.swift
+//  rently
+//
+//  Created by Sara Riyad on 11/8/24.
+//
+
+import Foundation
+import SwiftUI
+import FirebaseFirestore
+
+
+class ListingDetailViewModel: ObservableObject {
+    @Published var listing: Listing?
+    private let db = Firestore.firestore()
+    
+  
+  func fetchListing(by id: UUID) {
+      let docRef = db.collection("listings").document(id.uuidString)
+      
+      docRef.getDocument { (document, error) in
+          if let document = document, document.exists {
+              let data = document.data()
+
+              // Updated to reflect the new model
+              self.listing = Listing(
+                  id: document.documentID,
+                  title: data?["title"] as? String ?? "Untitled",
+                  creationTime: data?["creationTime"] as? Date ?? Date(),
+                  description: data?["description"] as? String ?? "",
+                  category: Category(rawValue: data?["category"] as? String ?? "") ?? .womensTops,
+                  userID: data?["userID"] as? String ?? "",
+                  size: ItemSize(rawValue: data?["size"] as? String ?? "medium") ?? .medium,
+                  price: data?["price"] as? Double ?? 0.0,
+                  color: ItemColor(rawValue: data?["color"] as? String ?? "blue") ?? .blue,
+                  condition: Condition(rawValue: data?["condition"] as? String ?? "") ?? .good,
+                  photoURLs: data?["photoURLs"] as? [String] ?? [],
+                  tags: (data?["tags"] as? [String])?.compactMap { TagOption(rawValue: $0) } ?? [],
+                  brand: data?["brand"] as? String ?? "",
+                  maxRentalDuration: RentalDuration(rawValue: data?["maxRentalDuration"] as? String ?? "oneMonth") ?? .oneMonth,
+                  pickupLocations: (data?["pickupLocations"] as? [String])?.compactMap { PickupLocation(rawValue: $0) } ?? [],
+                  available: data?["available"] as? Bool ?? false
+              )
+          } else {
+              print("Document does not exist")
+          }
+      }
+  }
+
+
+}
---
+++ rently/ViewModels/SearchViewModel.swift
@@ -0,0 +1,52 @@
+//
+//  SearchViewModel.swift
+//  rently
+//
+//  Created by Sara Riyad on 11/2/24.
+//
+
+import SwiftUI
+import FirebaseFirestore
+
+class SearchViewModel: ObservableObject {
+    @Published var listings = [Listing]()
+    @Published var users = [User]()
+    private let db = Firestore.firestore()
+    
+  func fetchListings() {
+      db.collection("listings").getDocuments { snapshot, error in
+          if let error = error {
+              print("Error fetching listings: \(error)")
+              return
+          }
+          
+          self.listings = snapshot?.documents.map { document in
+              let data = document.data()
+              let uuid = UUID(uuidString: document.documentID) ?? UUID()
+
+              // Updated to reflect the new model
+              return Listing(
+                  id: document.documentID,
+                  title: data["title"] as? String ?? "Untitled",
+                  creationTime: data["creationTime"] as? Date ?? Date(),
+                  description: data["description"] as? String ?? "",
+                  category: Category(rawValue: data["category"] as? String ?? "") ?? .womensTops,
+                  userID: data["userID"] as? String ?? "",
+                  size: ItemSize(rawValue: data["size"] as? String ?? "medium") ?? .medium,
+                  price: data["price"] as? Double ?? 0.0,
+                  color: ItemColor(rawValue: data["color"] as? String ?? "blue") ?? .blue,
+                  condition: Condition(rawValue: data["condition"] as? String ?? "") ?? .good,
+                  photoURLs: data["photoURLs"] as? [String] ?? [],
+                  tags: (data["tags"] as? [String])?.compactMap { TagOption(rawValue: $0) } ?? [],
+                  brand: data["brand"] as? String ?? "",
+                  maxRentalDuration: RentalDuration(rawValue: data["maxRentalDuration"] as? String ?? "oneMonth") ?? .oneMonth,
+                  pickupLocations: (data["pickupLocations"] as? [String])?.compactMap { PickupLocation(rawValue: $0) } ?? [],
+                  available: data["available"] as? Bool ?? false
+              )
+          } ?? []
+      }
+  }
+
+  
+}
+

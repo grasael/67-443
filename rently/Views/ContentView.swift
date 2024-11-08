be7f1d1cf6fb91970df9a47b8da89e5f957fe33b
Grace <gracelia@andrew.cmu.edu>
2024-11-08 12:46:23 -0500

    Added back functionality for sign up

--- rently/Views/ContentView.swift
+++ rently/Views/ContentView.swift
@@ -8,25 +8,16 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var isOnboardingComplete = false
-    @State private var isSignUpComplete = false
     @State private var user: User? // Store the newly created user after onboarding
 
     var body: some View {
         if isOnboardingComplete, let currentUser = user {
-            // Show the main app interface with tabs once onboarding is complete
             AppView(user: currentUser)
         } else {
-            // Show the onboarding view initially
             OnboardingView(onboardingComplete: { newUser in
-                // Set user data and mark onboarding as complete
                 self.user = newUser
                 self.isOnboardingComplete = true
             })
-//            WelcomeView(onSignUpComplete: { newUser in
-//                // Set the user data and mark onboarding as complete
-//                self.user = newUser
-//                self.isOnboardingComplete = true
-//            })
         }
     }
 }

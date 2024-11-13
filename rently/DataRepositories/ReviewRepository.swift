a9f831ed4bac29ec279ceea4ff995a1327747e8b
nobodyg0n3 <124393186+nobodyg0n3@users.noreply.github.com>
2024-11-13 01:10:06 -0500

    redid commits to include dynamic search

---
+++ rently/DataRepositories/ReviewRepository.swift
@@ -0,0 +1,73 @@
+//
+//  ReviewRepository.swift
+//  rently
+//
+//  Created by Grace Liao on 11/7/24.
+//
+import Foundation
+import Combine
+import FirebaseFirestore
+import FirebaseAuth
+
+class ReviewRepository: ObservableObject {
+    private let path: String = "Reviews"
+    private let store = Firestore.firestore()
+    
+    @Published var reviews: [Review] = []
+    private var cancellables: Set<AnyCancellable> = []
+
+    init() {
+        self.get()
+    }
+
+    func get() {
+        guard let userID = Auth.auth().currentUser?.uid else {
+            print("No user is logged in")
+            return
+        }
+        
+        store.collection(path)
+            .whereField("userID", isEqualTo: userID) // Filter reviews by the current user's ID
+            .addSnapshotListener { querySnapshot, error in
+                if let error = error {
+                    print("Error getting reviews: \(error.localizedDescription)")
+                    return
+                }
+                
+                self.reviews = querySnapshot?.documents.compactMap { document in
+                    try? document.data(as: Review.self)
+                } ?? []
+            }
+    }
+
+    // MARK: CRUD methods
+    func create(_ review: Review) {
+        do {
+            var newReview = review
+            newReview.userID = Auth.auth().currentUser!.uid // Set user ID for the review
+            _ = try store.collection(path).addDocument(from: newReview)
+        } catch {
+            fatalError("Unable to add review: \(error.localizedDescription).")
+        }
+    }
+
+    func update(_ review: Review) {
+        guard let reviewId = review.id else { return }
+        
+        do {
+            try store.collection(path).document(reviewId).setData(from: review)
+        } catch {
+            fatalError("Unable to update review: \(error.localizedDescription).")
+        }
+    }
+
+    func delete(_ review: Review) {
+        guard let reviewId = review.id else { return }
+        
+        store.collection(path).document(reviewId).delete { error in
+            if let error = error {
+                print("Unable to remove review: \(error.localizedDescription)")
+            }
+        }
+    }
+}
---
+++ rently/ListingsViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  ListingsViewModel.swift
+//  rently
+//
+//  Created by Abby Chen on 11/2/24.
+//
+
+import Foundation
+import SwiftUI
+
+class ListingsViewModel: ObservableObject {
+  @Published var listings: [Listing] = []
+  
+  // add listing
+  func addListing() {
+    
+  }
+  
+  // edit listing
+  
+  // delete listing
+}

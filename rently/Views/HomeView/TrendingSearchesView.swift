85e5688c69f1c7a7ce0f880b215443184af792ba
Tishyaa Chaudhry <tishyaa.chaudhry@gmail.com>
2024-11-20 08:13:14 -0500

    made trending searches clickable

--- rently/Views/HomeView/TrendingSearchesView.swift
+++ rently/Views/HomeView/TrendingSearchesView.swift
@@ -19,19 +19,25 @@ struct TrendingSearchesView: View {
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(spacing: 20) {
                     ForEach(viewModel.trendingItems) { item in
-                        VStack {
-                            AsyncImage(url: URL(string: item.thumbnailUrl)) { image in
-                                image
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fill)
-                                    .frame(width: 80, height: 80)
-                                    .clipShape(Circle())
-                            } placeholder: {
-                                Circle()
-                                    .fill(Color.gray.opacity(0.2))
-                                    .frame(width: 80, height: 80)
+                        // Make each item clickable, redirecting to the SearchView with the query
+                        NavigationLink(destination: SearchView(query: item.name)) {
+                            VStack {
+                                AsyncImage(url: URL(string: item.thumbnailUrl)) { image in
+                                    image
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .frame(width: 80, height: 80)
+                                        .clipShape(Circle())
+                                } placeholder: {
+                                    Circle()
+                                        .fill(Color.gray.opacity(0.2))
+                                        .frame(width: 80, height: 80)
+                                }
+                                Text(item.name)
+                                    .font(.subheadline)
+                                    .foregroundColor(.primary)
                             }
-                            Text(item.name)
+                            .padding(.vertical, 5)
                         }
                     }
                 }

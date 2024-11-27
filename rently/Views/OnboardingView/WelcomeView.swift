f25d4c8870fbbff8322cb59a4c692cba1f7a8daf
Grace <gracelia@andrew.cmu.edu>
2024-11-27 12:31:19 -0500

    Improved onboarding: Added welcome screen + navigation

--- rently/Views/OnboardingView/WelcomeView.swift
+++ rently/Views/OnboardingView/WelcomeView.swift
@@ -6,3 +6,65 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Spacer()
+
+                Text("rently")
+                    .font(.system(size: 60, weight: .bold))
+                    .foregroundColor(Color.blue.opacity(0.8))
+                    .shadow(radius: 2)
+                    .padding()
+
+                Spacer()
+
+                NavigationLink(destination: OnboardingView { _ in }) {
+                    Text("get started")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(
+                            LinearGradient(
+                                gradient: Gradient(colors: [Color.blue.opacity(0.4), Color.green.opacity(0.8)]),
+                                startPoint: .leading,
+                                endPoint: .trailing
+                            )
+                        )
+                        .cornerRadius(20)
+                        .padding(.horizontal, 50)
+                }
+                .padding(.bottom, 10)
+
+                NavigationLink(destination: SignInView()) {
+                    Text("I already have an account")
+                        .font(.subheadline)
+                        .foregroundColor(.black)
+                        .underline()
+                }
+                .padding(.bottom, 40)
+
+                Spacer()
+            }
+            .background(
+                LinearGradient(
+                    gradient: Gradient(colors: [Color(red: 0.9, green: 0.95, blue: 1), Color.white]),
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+                .ignoresSafeArea()
+            )
+        }
+        .navigationBarHidden(true)
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}

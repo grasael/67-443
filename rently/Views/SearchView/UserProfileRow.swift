b7b977764a24c3a59e44883514165369650405c5
Grace <gracelia@andrew.cmu.edu>
2024-12-12 06:03:28 -0500

    Updated SearchUserDetailView UI to look more similar to ProfileView

--- rently/Views/SearchView/UserProfileRow.swift
+++ rently/Views/SearchView/UserProfileRow.swift
@@ -10,18 +10,36 @@ import SwiftUI
 
 struct UserProfileRow: View {
     var user: User
-    
+
     var body: some View {
         HStack {
-            Text(user.firstName + user.lastName)
-                .font(.headline)
-            Spacer()
-            Text(user.username)
-                .font(.subheadline)
+            // Profile Picture
+            Image(systemName: "person.circle.fill")
+                .resizable()
+                .frame(width: 50, height: 50)
                 .foregroundColor(.gray)
+
+            // User Information
+            VStack(alignment: .leading) {
+                Text("\(user.firstName) \(user.lastName)")
+                    .font(.headline)
+                Text("@\(user.username)")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+            }
+            Spacer()
+
+            // Rating
+            HStack(spacing: 2) {
+                Text("\(user.rating, specifier: "%.1f")")
+                Image(systemName: "star.fill")
+                    .foregroundColor(.yellow)
+                    .font(.caption)
+            }
         }
         .padding()
+        .background(Color(UIColor.systemGray6))
+        .cornerRadius(10)
+        .shadow(radius: 1)
     }
 }
-
-
